27d4fa0dca92e6c68464dba2ab5779381ad8e3c7
lishuzhen <lishuzhen5678@163.com>
2019-12-13 11:57:29 +0800

    init

---
+++ src/com/lsz/map/TestMain.java
@@ -0,0 +1,12 @@
+package com.lsz.map;
+
+/**
+ * @description:
+ * @author: Lishuzhen
+ * @create: 2019-12-13 11:56
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
